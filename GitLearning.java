eee88b6f9120627c442d5e99b37326e56b0f0fe6
Munir Zikrullaev <munirprog@gmail.com>
2021-06-11 10:43:31 +0300

    add feature to GitLearning.java

--- GitLearning.java
+++ GitLearning.java
@@ -1,5 +1,11 @@
 public class GitLearning{
 	public static void main(String[] args){
 		System.out.println("Hello from GitLearning!");
+		callFeature();
 	}
+	
+	public static void callFeature(){
+                System.out.println("Hello from add-feature1");
+        }
+
 }
